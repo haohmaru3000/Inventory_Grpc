e4122c6786e8d757e40b78abf3ca04b1a5d2ba76
haohmaru3000 <tuanit168@gmail.com>
2025-03-17 16:45:05 +0700

    7. First setup of gRPC service for Inventory microservice

---
+++ Inventory.Grpc/Extensions/ServiceExtensions.cs
@@ -0,0 +1,43 @@
+using Infrastructure.Extensions;
+using Inventory.Grpc.Repositories;
+using Inventory.Grpc.Repositories.Interfaces;
+using MongoDB.Driver;
+using Shared.Configurations;
+
+namespace Inventory.Grpc.Extensions;
+
+public static class ServiceExtensions
+{
+    internal static IServiceCollection AddConfigurationSettings(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var databaseSettings = configuration.GetSection(nameof(MongoDbSettings))
+            .Get<MongoDbSettings>();
+        if (databaseSettings != null) services.AddSingleton(databaseSettings);
+
+        return services;
+    }
+
+    private static string GetMongoConnectionString(this IServiceCollection services)
+    {
+        var settings = services.GetOptions<MongoDbSettings>(nameof(MongoDbSettings));
+        if (settings == null || string.IsNullOrEmpty(settings.ConnectionString))
+            throw new ArgumentNullException("MongoDbSettings is not configured.");
+
+        var databaseName = settings.DatabaseName;
+        var mongoDbConnectionString = settings.ConnectionString + "/" + databaseName + "?authSource=admin";
+        return mongoDbConnectionString;
+    }
+
+    // Config Mongo Client to be able to connect to MongoDB
+    public static void ConfigureMongoDbClient(this IServiceCollection services)
+    {
+        services.AddSingleton<IMongoClient>(new MongoClient(GetMongoConnectionString(services)))
+            .AddScoped(x => x.GetService<IMongoClient>()?.StartSession());
+    }
+
+    public static void AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IInventoryRepository, InventoryRepository>();
+    }
+}
